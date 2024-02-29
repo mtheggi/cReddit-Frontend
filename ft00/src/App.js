4a39da7c1363eadd6da59d1bab6bdf9644d93f70
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-02-29 10:49:31 +0200

    SP00-Repo Init

---
+++ ft00/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
