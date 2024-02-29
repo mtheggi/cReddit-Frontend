ef2cc06af7cdbbc9ba17acaf6060ab5c930536ee
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-02-29 15:17:10 +0200

    Migrating to vite

---
+++ ft00/src/App.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ft00.css';
+
+import Navbar from './Components/Navbar';
+
+
+function App() {
+
+
+  return (
+
+     <div className="App">
+          <Navbar />
+
+    </div>
+
+  )
+}
+
+export default App
