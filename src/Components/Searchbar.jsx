1ecb2c195a681414ce9b24fc33daecaeb97466bb
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-03-01 19:36:59 +0200

    Modify the Repo Structure

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -23,9 +23,9 @@ const Searchbar = () => {
 
     return (
 
-        <form action="" className={`bg-reddit_search h-10 min-w-24 min-h-10 items-center flex flex-grow rounded-full px-2.5  ${isHovered ? ' bg-reddit_search_light' : ''}`}>
-            <MagnifyingGlassIcon className="text-gray-300 h-5 w-6 min-h-5 min-w-6 mr-1" />
-            <input type="text" className="bg-reddit_search h-7 w-11/12 text-sm font-extralight text-white focus:outline-none hover:bg-reddit_search_light"  placeholder={placeholder}
+        <form action="" className={`xs:bg-reddit_search h-10 xs:min-w-24 min-h-10 items-center flex xs:flex-grow rounded-full px-1 xs:px-2.5  ${isHovered ? ' bg-reddit_search_light' : ''}`}>
+            <MagnifyingGlassIcon className="text-gray-300 xs:h-5 xs:w-6 h-7 w-6 min-h-5 min-w-6  ml-3 xs:ml-0 xs:mr-1" />
+            <input type="text" className="bg-reddit_search h-7 w-11/12 text-sm hidden xs:block font-extralight text-white focus:outline-none hover:bg-reddit_search_light"  placeholder={placeholder}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)} />
         </form>
