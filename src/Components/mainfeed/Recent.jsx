7bfa998cdf29447dc0712b1c5632caf2e84d3b16
Mohamed Heggi <mtheggi@gmail.com>
2024-03-10 22:30:34 +0200

    userprofile card added temperoray under recent in homepgage
    responsive userprofile card is added
    added in recent.jsx in the homepage
    responsive

--- src/Components/mainfeed/Recent.jsx
+++ src/Components/mainfeed/Recent.jsx
@@ -1,36 +1,37 @@
 import Separator from '../sidebar/Nav-Icons/Separator';
 import RecentRow from './RecentRow';
-
+import Usercard from '../usercard/Usercard';
 const Recent = () => {
     return (
+        <>
+            <div className=' bg-reddit_darkRecent hidden lg:flex flex-col h-fit pt-3 pb-1 mb-10 rounded-2xl w-90 xl:w-80 ml-3 mt-9 mr-auto'>
+                <div className=' h-6 w-full flex items-center px-3 flex-row'>
+                    <p className=' text-xs text-gray-400 font-medium lette tracking-widest '>RECENT POSTS</p>
+                    <p className=' text-sm text-blue-400 font-normal cursor-pointer hover:underline lette no-select ml-auto '>Clear</p>
+                </div>
 
-        <div className=' bg-reddit_darkRecent hidden lg:flex flex-col h-fit pt-3 pb-1 mb-10 rounded-2xl w-90 xl:w-80 ml-3 mt-9 mr-auto'>
-            <div className=' h-6 w-full flex items-center px-3 flex-row'>
-                <p className=' text-xs text-gray-400 font-medium lette tracking-widest '>RECENT POSTS</p>
-                <p className=' text-sm text-blue-400 font-normal cursor-pointer hover:underline lette no-select ml-auto '>Clear</p>
+
+                {/* repeated part */}
+                <div className=' space-y-4'>
+                    <RecentRow />
+                    <Separator />
+                    <RecentRow />
+                    <Separator />
+                    <RecentRow />
+                    <RecentRow />
+                    <Separator />
+                    <RecentRow />
+                    <Separator />
+                    <RecentRow />
+                    <Separator />
+                    <RecentRow />
+                    <Separator />
+                    <RecentRow />
+                </div>
+
             </div>
-
-
-            {/* repeated part */}
-            <div className=' space-y-4'>
-                <RecentRow />
-                <Separator />
-                <RecentRow />
-                <Separator />
-                <RecentRow />
-                <RecentRow />
-                <Separator />
-                <RecentRow />
-                <Separator />
-                <RecentRow />
-                <Separator />
-                <RecentRow />
-                <Separator />
-                <RecentRow />
-            </div>
-
-        </div>
-
+            <Usercard />
+        </>
     );
 }
 
