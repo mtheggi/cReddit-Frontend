47b76d7d7bee2e7011001213ab312e11d608a5f8
Abdelaal <ahmmohabd2001@gmail.com>
2024-03-16 15:59:27 +0200

    Adjusted Ids

--- src/Components/settings/Emails.jsx
+++ src/Components/settings/Emails.jsx
@@ -2,18 +2,6 @@ import React, { useState } from "react";
 import Setting from "./Setting";
 import Subtitle from "./components/Subtitle";
 
-const emailSettings = {
-  messages: [{ id: "chatRequests", title: "Chat requests" }],
-  activity: [{ id: "newFollowers", title: "New followers" }],
-  finalEmail: [
-    {
-      id: "unsubscribeFromAllEmails",
-      title: "Unsubscribe from all emails",
-      initialValue: false,
-    },
-  ],
-};
-
 function Emails() {
   const [toggles, setToggles] = useState({
     chatRequests: true,
@@ -28,31 +16,47 @@ function Emails() {
     }));
   };
 
-  const renderSection = (section, title) => (
-    <>
-      <Subtitle title={title.toUpperCase()} />
-      {section.map((setting) => (
-        <Setting
-          key={setting.id}
-          title={setting.title}
-          toggleButton={true}
-          isToggled={toggles[setting.id]}
-          toggleButtonOnClick={() => handleToggle(setting.id)}
-        />
-      ))}
-    </>
-  );
-
   return (
     <div className="flex flex-col w-full">
       <h3 className="text-white text-xl -mb-3 font-bold font-plex">
         Manage Emails
       </h3>
-      {renderSection(emailSettings.messages, "Messages")}
-      {renderSection(emailSettings.activity, "Activity")}
-      {renderSection(emailSettings.finalEmail, "")}
+      <Subtitle title = "Messages" />
+      <Setting
+          id = "settings-emails-category-chat-requests-toggle-button"
+          key = "chatRequests"
+          title = "Chat requests"
+          toggleButton = {true}
+          isToggled = {toggles["chatRequests"]}
+          toggleButtonOnClick={() => handleToggle("chatRequests")}
+      />
+
+      <Subtitle title = "Activity" />
+      <Setting
+          id = "settings-emails-category-new-followers-toggle-button"
+          key = "newFollowers"
+          title = "New followers"
+          toggleButton = {true}
+          isToggled = {toggles["newFollowers"]}
+          toggleButtonOnClick={() => handleToggle("newFollowers")}
+      />
+
+      <Subtitle title = "" />
+      <Setting
+          id = "settings-emails-category-unsubscribe-from-all-emails-toggle-button"
+          key = "unsubscribeFromAllEmails"
+          title = "Unsubscribe from all emails"
+          toggleButton = {true}
+          isToggled = {toggles["unsubscribeFromAllEmails"]}
+          toggleButtonOnClick={() => handleToggle("unsubscribeFromAllEmails")}
+      />
     </div>
   );
 }
 
 export default Emails;
+
+/* ID Documentation */
+// settings-emails-category-chat-requests-toggle-button: Toggle Button for chat requests
+// settings-emails-category-new-followers-toggle-button: Toggle Button for new followers
+// settings-emails-category-unsubscribe-from-all-emails-toggle-button: Toggle Button for unsubscribing from all emails
--- src/Components/settings/Notifications.jsx
+++ src/Components/settings/Notifications.jsx
@@ -2,19 +2,6 @@ import React, { useState } from "react";
 import Setting from "./Setting";
 import Subtitle from "./components/Subtitle";
 
-const notificationsSettings = {
-  activity: [
-    { id: "mentionsOfUsername", title: "Mentions of u/username" },
-    { id: "commentsOnYourPosts", title: "Comments on your posts" },
-    { id: "upvotesOnYourPosts", title: "Upvotes on your posts" },
-    { id: "upvotesOnYourComments", title: "Upvotes on your comments" },
-    { id: "repliesToYourComments", title: "Replies to your comments" },
-    { id: "newFollowers", title: "New followers" },
-    { id: "postsYouFollow", title: "Posts you follow" },
-    { id: "commentsYouFollow", title: "Comments you follow" },
-  ],
-};
-
 function Notifications() {
   const [toggles, setToggles] = useState({
     mentionsOfUsername: true,
@@ -34,29 +21,97 @@ function Notifications() {
     }));
   };
 
-  const renderSection = (section, title) => (
-    <>
-      <Subtitle title={title.toUpperCase()} />
-      {section.map((setting) => (
-        <Setting
-          key={setting.id}
-          title={setting.title}
-          toggleButton={true}
-          isToggled={toggles[setting.id]}
-          toggleButtonOnClick={() => handleToggle(setting.id)}
-        />
-      ))}
-    </>
-  );
-
   return (
     <div className="flex flex-col w-full">
       <h3 className="text-white -mb-3 text-xl font-bold font-plex">
         Notification settings
       </h3>
-      {renderSection(notificationsSettings.activity, "Activity")}
+
+      <Subtitle title = "Activity" />
+      
+      <Setting
+          id = "settings-notifications-category-mentions-of-username-toggle-button"
+          key="mentionsOfUsername"
+          title="Mentions of u/username"
+          toggleButton={true}
+          isToggled={toggles["mentionsOfUsername"]}
+          toggleButtonOnClick={() => handleToggle("mentionsOfUsername")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-comments-on-your-posts-toggle-button"
+          key="commentsOnYourPosts"
+          title="Comments on your posts"
+          toggleButton={true}
+          isToggled={toggles["commentsOnYourPosts"]}
+          toggleButtonOnClick={() => handleToggle("commentsOnYourPosts")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-upvotes-on-your-posts-toggle-button"
+          key="upvotesOnYourPosts"
+          title="Upvotes on your posts"
+          toggleButton={true}
+          isToggled={toggles["upvotesOnYourPosts"]}
+          toggleButtonOnClick={() => handleToggle("upvotesOnYourPosts")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-upvotes-on-your-comments-toggle-button"
+          key="upvotesOnYourComments"
+          title="Upvotes on your comments"
+          toggleButton={true}
+          isToggled={toggles["upvotesOnYourComments"]}
+          toggleButtonOnClick={() => handleToggle("upvotesOnYourComments")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-replies-to-your-comments-toggle-button"
+          key="repliesToYourComments"
+          title="Replies to your comments"
+          toggleButton={true}
+          isToggled={toggles["repliesToYourComments"]}
+          toggleButtonOnClick={() => handleToggle("repliesToYourComments")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-new-followers-toggle-button"
+          key="newFollowers"
+          title="New followers"
+          toggleButton={true}
+          isToggled={toggles["newFollowers"]}
+          toggleButtonOnClick={() => handleToggle("newFollowers")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-posts-you-follow-toggle-button"
+          key="postsYouFollow"
+          title="Posts you follow"
+          toggleButton={true}
+          isToggled={toggles["postsYouFollow"]}
+          toggleButtonOnClick={() => handleToggle("postsYouFollow")}
+      />
+
+      <Setting
+          id = "settings-notifications-category-comments-you-follow-toggle-button"
+          key="commentsYouFollow"
+          title="Comments you follow"
+          toggleButton={true}
+          isToggled={toggles["commentsYouFollow"]}
+          toggleButtonOnClick={() => handleToggle("commentsYouFollow")}
+      />
     </div>
   );
 }
 
 export default Notifications;
+
+/* ID Documentation */
+// settings-notifications-category-mentions-of-username-toggle-button: Toggle Button for mentions of username
+// settings-notifications-category-comments-on-your-posts-toggle-button: Toggle Button for comments on your posts
+// settings-notifications-category-upvotes-on-your-posts-toggle-button: Toggle Button for upvotes on your posts
+// settings-notifications-category-upvotes-on-your-comments-toggle-button: Toggle Button for upvotes on your comments
+// settings-notifications-category-replies-to-your-comments-toggle-button: Toggle Button for replies to your comments
+// settings-notifications-category-new-followers-toggle-button: Toggle Button for new followers
+// settings-notifications-category-posts-you-follow-toggle-button: Toggle Button for posts your follow
+// settings-notifications-category-comments-you-follow-toggle-button: Toggle Button for comments you follow
