094904fa49a440f160ec4f9d4a921b836f2bc9ba
Mohamed Heggi <mtheggi@gmail.com>
2024-03-18 20:34:34 +0200

    Axios Interceptor + redoing the Original Request

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useState, useRef, useEffect } from 'react';
 import Settings from './Components/settings/Settings';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { FormText } from 'react-bootstrap';
+import axiosInterceptor from './utils/axiosInterceptor';
+
 function App() {
   const [isVisibleLeftSidebar, setIsVisibleLeftSidebar] = useState(false);
   const navbarRef = useRef();
