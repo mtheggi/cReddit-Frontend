c07b3b46d99f757b1fe2ec015e95afe2a6ce1727
Master60 <habebelrhman321@gmail.com>
2024-04-19 22:02:22 +0200

    Allowing Sidebar to be displayed in TopCommunities Page

--- src/Components/topcommunities/TopCommunities.jsx
+++ src/Components/topcommunities/TopCommunities.jsx
@@ -1,13 +1,33 @@
-import Navbar from "../navbar/Navbar";
+import { useContext } from "react";
 import CommunitiesSection from "./CommunitiesSection";
+import { SidebarContext } from "@/context/SidebarContext";
+import Sidebar from "../sidebar/Sidebar";
 
 /**
  * React component representing a page displaying top communities.
  * @returns {JSX.Element} React component.
  */
-const TopCommunities = () => {
+const TopCommunities = ({ isVisibleLeftSidebar, setIsVisibleLeftSidebar }) => {
+  const { setIsCommunityOpen, communityButtonRef, userHistoryRes, sidebarRef } =
+    useContext(SidebarContext);
+
   return (
     <>
+      <div
+        ref={sidebarRef}
+        className={`h-full ${
+          isVisibleLeftSidebar
+            ? "fixed left-0 xl:relative xl:flex pl-1 bg-reddit_navbar w-70"
+            : "hidden xl:flex"
+        } z-20 w-66 min-w-60 border-r border-neutral-800 pt-2 mr-2 no-select ml-auto overflow-auto scrollbar_mod overflow-x-hidden`}
+      >
+        <Sidebar
+          setIsCommunityOpen={setIsCommunityOpen}
+          communityButtonRef={communityButtonRef}
+          setIsVisibleLeftSidebar={setIsVisibleLeftSidebar}
+          userHistoryRes={userHistoryRes}
+        />
+      </div>
       <div id="Top-Communities" className="bg-[#0b1416] min-w-[350px]">
         <CommunitiesSection />
       </div>
