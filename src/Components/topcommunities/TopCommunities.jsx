58fe8a5c9751d96c6116164ff82c03fa82ec3e3c
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-04-20 18:38:31 +0200

    Temprorary sidebar fix

--- src/Components/topcommunities/TopCommunities.jsx
+++ src/Components/topcommunities/TopCommunities.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import CommunitiesSection from "./CommunitiesSection";
 import { SidebarContext } from "@/context/SidebarContext";
 import Sidebar from "../sidebar/Sidebar";
@@ -16,6 +16,10 @@ const TopCommunities = ({ isVisibleLeftSidebar, setIsVisibleLeftSidebar }) => {
   const { setIsCommunityOpen, communityButtonRef, userHistoryRes, sidebarRef } =
     useContext(SidebarContext);
 
+    useEffect(() => {
+     setIsVisibleLeftSidebar(false);
+    } ,[]);
+
   return (
     <>
       <div
@@ -23,7 +27,7 @@ const TopCommunities = ({ isVisibleLeftSidebar, setIsVisibleLeftSidebar }) => {
         className={`h-full ${
           isVisibleLeftSidebar
             ? "fixed left-0 xl:relative xl:flex pl-1 bg-reddit_navbar w-70"
-            : "hidden xl:flex"
+            : "hidden"
         } z-20 w-66 min-w-60 border-r border-neutral-800 pt-2 mr-2 no-select ml-auto overflow-auto scrollbar_mod overflow-x-hidden`}
       >
         <Sidebar
