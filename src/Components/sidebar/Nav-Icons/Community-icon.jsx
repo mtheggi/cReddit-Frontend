aa0fe0184d2e792e4e896d4507198ad9f5639462
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-05-01 00:06:11 +0300

    remove unneeded bookmark

--- src/Components/sidebar/Nav-Icons/Community-icon.jsx
+++ src/Components/sidebar/Nav-Icons/Community-icon.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import propsTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 EmptyStart.propTypes = {
     color: propsTypes.string
@@ -56,19 +57,20 @@ function EmptyStart({ color, isBookmarked }) {
 
 const CommunityIcon = ({ text, divId, bookmarkId, icon, href }) => {
 
-    const [isBookmarked, setIsHisBookmarkedover] = useState(false)
+    // const [isBookmarked, setIsHisBookmarkedover] = useState(false);
+    const navigate = useNavigate();
     return (
-        <Link to={href} id={divId} data-testid={divId} className="testClass flex h-[48px] flex-row ml-0.5  justify-between pl-3 pr-4 hover:bg-reddit_hover SideIcon-Container rounded-lg items-center cursor-pointer">
+        <div onClick={()=>navigate(href)} id={divId} data-testid={divId} className="testClass flex h-[48px] mb-1 flex-row ml-0.5  justify-between pl-3 pr-4 hover:bg-reddit_hover SideIcon-Container rounded-lg items-center cursor-pointer">
             <div className="flex flex-row items-center justify-start">
                 <img src={icon} className="h-[30px] w-[32px] rounded-full" alt="randomImgs" />
-                <span className="text-gray-200 text-[13px] tracking-wider letter font-normal ml-3">{text}</span>
+                <span className="text-gray-200 text-[13px] tracking-wider truncate letter font-normal ml-3">{text}</span>
             </div>
 
-            <div onClick={() => setIsHisBookmarkedover((prev) => !prev)} id={bookmarkId} className="flex justify-center items-center w-8 h-8 rounded-full  hover:bg-reddit_search_light">
+            {/* <div onClick={(e) =>{ e.stopPropagation(); setIsHisBookmarkedover((prev) => !prev)}} id={bookmarkId} className="flex justify-center items-center w-8 h-8 rounded-full  hover:bg-reddit_search_light">
                 <span className="h-5 w-5 flex justify-center items-center " role="button"> <EmptyStart color={"white"} isBookmarked={isBookmarked} /> </span>
-            </div>
+            </div> */}
 
-        </Link>
+        </div>
     );
 }
 CommunityIcon.propTypes = {
