bd552eb70589320029788c5b13b8451ef4e8a17e
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-05-01 12:13:54 +0300

    Add share copy link feature

--- src/Components/mainfeed/Share.jsx
+++ src/Components/mainfeed/Share.jsx
@@ -7,9 +7,10 @@ import { ArrowUpTrayIcon } from "@heroicons/react/24/outline";
  * @param {string} props.id - The ID of the post
  * @returns {JSX.Element} The rendered Share component
  */
-const Share = ({id, testId=""}) => {
+const Share = ({id, testId="", setIsShareMenuOpened}) => {
     return (
-        <div id={"mainfeed_"+id+"_share" + testId} className="flex flex-row items-center cursor-pointer w-21 h-8 justify-center  bg-reddit_search hover:bg-reddit_search_light rounded-3xl ">
+        <div onClick={()=>setIsShareMenuOpened(prev=>!prev)} 
+         id={"mainfeed_"+id+"_share" + testId} className="flex flex-row items-center cursor-pointer w-21 h-8 justify-center  bg-reddit_search hover:bg-reddit_search_light rounded-3xl ">
             <ArrowUpTrayIcon className="h-5 w-6 mr-1 text-gray-300" />
             <span className="text-gray-300 text-sm mr-1.5"> Share</span>
         </div>
