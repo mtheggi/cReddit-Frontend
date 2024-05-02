180709f67579ebcb53a40f838f97ded859082c4e
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-05-02 18:07:38 +0300

    Make no dups in recents

--- src/Components/sidebar/Nav-Icons/DropDownMenu.jsx
+++ src/Components/sidebar/Nav-Icons/DropDownMenu.jsx
@@ -51,6 +51,7 @@ const DropDownMenu = ({
   const [isResources] = useState(MenuHeader === "RESOURCES");
   const [isCommunity] = useState(MenuHeader === "COMMUNITIES");
   const [isRecent] = useState(MenuHeader === "RECENT");
+  const [isModeration] = useState(MenuHeader === "MODERATION");
   const { isLoggedIn } = useContext(UserContext);
   const [joinedSubreddits, setJoinedSubreddits] = useState(null);
   const [recentSubreddits, setRecentSubreddits] = useState(null);
@@ -68,17 +69,26 @@ const DropDownMenu = ({
     }
   };
 
+
+  const getModerationSubreddits = async () => {
+
+  };
+
   const getRecentsubreddits = () => {
     const userHistory = JSON.parse(localStorage.getItem("userHistory"));
     if (userHistory !== null) {
-      const recentSubreddits = userHistory
+      const recentSubredditsSet = new Set();
+      userHistory
         .filter((post) => post?.communityName !== null)
-        .map((post) => {
-          return {
+        .forEach((post) => {
+          const subreddit = {
             name: post.communityName,
             icon: post.profilePicture,
           };
+          recentSubredditsSet.add(JSON.stringify(subreddit));
         });
+
+      const recentSubreddits = Array.from(recentSubredditsSet).map(JSON.parse);
 
       setRecentSubreddits(recentSubreddits);
     }
@@ -95,7 +105,7 @@ const DropDownMenu = ({
     <>
       <div
         id={id}
-        className="min-h-15 w-full bg-reddit_greenyDark flex flex-row  relative items-center rounded-lg  "
+        className="min-h-14 w-full bg-reddit_greenyDark flex flex-row  relative items-center rounded-lg  "
       >
         <div className="flex h-13 pl-[0.5px] items-center w-full flex-row ">
           <div
@@ -133,6 +143,22 @@ const DropDownMenu = ({
           );
         })}
       </div>
+
+
+      <div className={`overflow-hidden space-y-1 transition-all duration-[300ms] max-h-0 opacity-0 ${isDropped && isModeration ? 'max-h-[600px] opacity-100' : ''}`}>
+
+        <div className="flex flex-col pb-1">
+          <div className="flex cursor-pointer flex-row hover:bg-reddit_hover h-11 px-3 rounded-lg items-center">
+            <svg rpl="" fill="white" height="21" icon-name="mod-queue-outline" viewBox="0 0 20 20" width="21" xmlns="http://www.w3.org/2000/svg">
+              <path d="m11.5 15.521-.158-.042C11.206 15.444 8 14.581 8 12.25V8.393l3.5-.914 3.5.914v3.857c0 2.331-3.206 3.194-3.342 3.229l-.158.042ZM9.25 9.357v2.893c0 1.147 1.713 1.8 2.249 1.972.536-.177 2.251-.83 2.251-1.972V9.357l-2.25-.586-2.25.586ZM17.375 19H5.625A1.627 1.627 0 0 1 4 17.375V5.625A1.627 1.627 0 0 1 5.625 4h11.75A1.627 1.627 0 0 1 19 5.625v11.75A1.627 1.627 0 0 1 17.375 19ZM5.625 5.25a.375.375 0 0 0-.375.375v11.75a.375.375 0 0 0 .375.375h11.75a.375.375 0 0 0 .375-.375V5.625a.375.375 0 0 0-.375-.375H5.625Zm-3 9.5a.375.375 0 0 1-.375-.375V2.624a.375.375 0 0 1 .375-.374h11.75a.375.375 0 0 1 .375.374H16A1.627 1.627 0 0 0 14.375 1H2.625A1.627 1.627 0 0 0 1 2.624v11.751A1.627 1.627 0 0 0 2.625 16v-1.25Z"></path>
+            </svg>
+            <h1 className="ml-[14px] text-[14px] text-gray-200">Mod Queue</h1>
+          </div>
+
+        </div>
+      </div>
+
+
 
       <div
         className={`overflow-hidden transition-all duration-[300ms] max-h-0 opacity-0 ${isDropped && isCommunity ? "max-h-[600px] opacity-100" : ""
@@ -200,7 +226,7 @@ const DropDownMenu = ({
         <NavIcon id={toSnakeCase("Topics")} href="#" text="Topics">
           <RectangleGroupIcon className="h-6 w-6 mr-2  text-gray-50" />
         </NavIcon>
-        <Separator color={500}  />
+        <Separator color={500} />
         <NavIcon
           id={toSnakeCase("Content Policy")}
           href="#"
