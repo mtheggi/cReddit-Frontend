ca866e9e38d74d70603172da16427a63a1371881
Master60 <habebelrhman321@gmail.com>
2024-05-04 23:57:53 +0300

    Comments Subsection Documentation

--- src/Components/viewprofile/Comments.jsx
+++ src/Components/viewprofile/Comments.jsx
@@ -4,20 +4,63 @@ import { baseUrl, limit } from "@/constants";
 import PostComment from "../mainfeed/comment/PostComment";
 import Loading from "../Loading/Loading";
 
+/**
+ * Renders a list of comments for a user.
+ * @module Comments
+ * @param {Object} props - The component props.
+ * @param {Object} props.userInfo - Information about the user.
+ * @returns {JSX.Element} A React component representing a list of comments.
+ */
 const Comments = ({ userInfo }) => {
+  /**
+   * State to hold the list of comments.
+   * @type {[Object[], Function]}
+   */
   const [comments, setComments] = useState([]);
+
+  /**
+   * State to track the current page of comments.
+   * @type {[number, Function]}
+   */
   const [currentPage, setCurrentPage] = useState(1);
+
+  /**
+   * State to indicate whether comments are being loaded.
+   * @type {[boolean, Function]}
+   */
   const [loading, setLoading] = useState(false);
+
+  /**
+   * State to indicate whether there are more comments to fetch.
+   * @type {[boolean, Function]}
+   */
   const [hasMore, setHasMore] = useState(true);
 
+  /**
+   * Ref to observe the last comment element for lazy loading.
+   * @type {Object}
+   */
   const observer = useRef();
+
+  /**
+   * Ref to the last comment element in the list.
+   * @type {Object}
+   */
   const lastCommentRef = useRef();
 
+  /**
+   * Function to load the next page of comments.
+   * @returns {void}
+   */
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
   };
 
   useEffect(() => {
+    /**
+     * Fetches comments from the server.
+     * @returns {void}
+     */
     const getComments = async () => {
       if (!hasMore) return;
       setLoading(true);
