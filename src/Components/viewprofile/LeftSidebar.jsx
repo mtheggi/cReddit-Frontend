c8410aa1d799c9be84e8d149a8d0040790275a3c
Master60 <habebelrhman321@gmail.com>
2024-05-04 23:59:02 +0300

    Left Sidebar Documentation

--- src/Components/viewprofile/LeftSidebar.jsx
+++ src/Components/viewprofile/LeftSidebar.jsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 import Sidebar from "../sidebar/Sidebar";
 
+/**
+ * Renders the left sidebar component.
+ * @module LeftSidebar
+ * @param {Object} props - The component props.
+ * @param {Object} props.sidebarProps - Properties related to the sidebar.
+ * @param {boolean} props.sidebarProps.isVisibleLeftSidebar - Indicates whether the left sidebar is visible.
+ * @param {Function} props.sidebarProps.setIsVisibleLeftSidebar - Function to set the visibility of the left sidebar.
+ * @param {Function} props.sidebarProps.setIsCommunityOpen - Function to set the visibility of the community.
+ * @param {Object} props.sidebarProps.communityButtonRef - Reference to the community button.
+ * @param {Object} props.sidebarProps.userHistoryRes - User history response.
+ * @param {Object} props.sidebarProps.sidebarRef - Reference to the sidebar.
+ * @returns {JSX.Element} A React component representing the left sidebar.
+ */
 const LeftSidebar = ({ sidebarProps }) => {
   const {
     isVisibleLeftSidebar,
@@ -11,16 +24,31 @@ const LeftSidebar = ({ sidebarProps }) => {
     sidebarRef,
   } = sidebarProps;
 
+  /**
+   * State to indicate whether the screen is considered large.
+   * @type {[boolean, Function]}
+   */
   const [isLargeScreen, setIsLargeScreen] = useState(true);
 
+  /**
+   * Function to check the screen width and update the state.
+   * @returns {void}
+   */
   const checkScreenWidth = () => {
     setIsLargeScreen(window.innerWidth > 1200);
   };
 
+  /**
+   * useEffect hook to set the visibility of the left sidebar to false when the component mounts.
+   */
   useEffect(() => {
     setIsVisibleLeftSidebar(false);
   }, []);
 
+  /**
+   * useEffect hook to check the screen width and add event listener for resize events to handle responsive behavior.
+   * Removes the event listener when the component unmounts.
+   */
   useEffect(() => {
     checkScreenWidth();
     window.addEventListener("resize", checkScreenWidth);
