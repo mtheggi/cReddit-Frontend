f1303dc16b6e5c7ade56ff3cc42a32e7313d0308
Master60 <habebelrhman321@gmail.com>
2024-05-04 20:57:22 +0300

    Creating the Posts subsection

---
+++ src/Components/viewprofile/Submitted.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState, useRef } from "react";
+import { getRequest } from "@/services/Requests";
+import { baseUrl, limit } from "@/constants";
+import Post from "../mainfeed/Post";
+import Loading from "../Loading/Loading";
+
+const Submitted = ({ userInfo }) => {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const observer = useRef();
+  const lastPostRef = useRef();
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  useEffect(() => {
+    const getPosts = async () => {
+      if (!hasMore) return;
+      setLoading(true);
+      try {
+        const response = await getRequest(
+          `${baseUrl}/user/${userInfo.username}/posts?page=${currentPage}&limit=${limit}`
+        );
+        if (response.status === 200 || response.status === 201) {
+          if (response.data.length === 0) {
+            setHasMore(false);
+          } else {
+            setPosts((prevPosts) => [...prevPosts, ...response.data]);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+      setLoading(false);
+    };
+
+    getPosts();
+  }, [currentPage, hasMore]);
+
+  useEffect(() => {
+    if (loading) return;
+
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          nextPage();
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (lastPostRef.current) {
+      observer.current.observe(lastPostRef.current);
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [loading, hasMore]);
+
+  return (
+    <>
+      {posts.map((post, index) => {
+        if (posts.length === index + 1) {
+          return (
+            <div key={post._id} ref={lastPostRef}>
+              <Post id={post._id} {...post} />
+            </div>
+          );
+        } else {
+          return <Post key={post._id} id={post._id} {...post} />;
+        }
+      })}
+
+      {loading && (
+        <div className="my-[24px]">
+          <Loading />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Submitted;
