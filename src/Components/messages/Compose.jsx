3281f7b54c1fbc5780dcc7d7161bddcd5ae628ec
Master60 <habebelrhman321@gmail.com>
2024-05-05 18:51:26 +0300

    Added Documentation for Compose

--- src/Components/messages/Compose.jsx
+++ src/Components/messages/Compose.jsx
@@ -1,5 +1,9 @@
 import MessagesCompose from "./MessagesCompose";
 
+/**
+ * Represents a component for composing messages.
+ * @returns {JSX.Element} The JSX element representing the compose component.
+ */
 const Compose = () => {
   return (
     <div id="compose" data-testId="compose">
