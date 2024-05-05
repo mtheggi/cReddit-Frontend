c9cde20702bc2dbe35a59324aab7f6331cd75404
Master60 <habebelrhman321@gmail.com>
2024-05-05 05:20:41 +0300

    Added Sidebar Context Provider in Index

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import axios from 'axios';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Client_ID, NODE_ENV } from './constants.js'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import { UserContextProvider } from './context/UserContext.jsx'
-import { ServerContextProvider } from './context/ServerContext.jsx'
-import { NavbarContextProvider } from './context/NavbarContext.jsx';
-import { StrictMode } from 'react';
-import { NotificationProvider } from './Components/notifications/NotificationContext';
-axios.interceptors.request.use(config => {
+import React from "react";
+import axios from "axios";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Client_ID, NODE_ENV } from "./constants.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { UserContextProvider } from "./context/UserContext.jsx";
+import { ServerContextProvider } from "./context/ServerContext.jsx";
+import { NavbarContextProvider } from "./context/NavbarContext.jsx";
+import { StrictMode } from "react";
+import { NotificationProvider } from "./Components/notifications/NotificationContext";
+import { SidebarContextProvider } from "./context/SidebarContext.jsx";
+
+axios.interceptors.request.use((config) => {
   config.withCredentials = true;
   return config;
 });
 
 async function enableMocking() {
-  if (NODE_ENV !== 'development') {
-    return
+  if (NODE_ENV !== "development") {
+    return;
   }
-  const { worker } = await import('./mocks/browser')
+  const { worker } = await import("./mocks/browser");
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start()
+  return worker.start();
 }
 enableMocking().then(() => {
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <GoogleOAuthProvider clientId={Client_ID} >
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <GoogleOAuthProvider clientId={Client_ID}>
       <NavbarContextProvider>
         <NotificationProvider>
           <ServerContextProvider>
             <UserContextProvider>
-
-              <App />
-
+              <SidebarContextProvider>
+                <App />
+              </SidebarContextProvider>
             </UserContextProvider>
           </ServerContextProvider>
         </NotificationProvider>
       </NavbarContextProvider>
-    </GoogleOAuthProvider >,
-  )
-})
-
+    </GoogleOAuthProvider>
+  );
+});
