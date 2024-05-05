c2144a4c6a410dd61fb5862917a1f3dfefa6bf1f
Master60 <habebelrhman321@gmail.com>
2024-05-05 04:54:10 +0300

    Restructuring Messages Page

--- src/Components/messages/Messages.jsx
+++ src/Components/messages/Messages.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { getRequest } from "@/services/Requests";
-import { baseUrl } from "@/constants";
-import MessagesHeader from "./MessagesHeader";
+import { baseUrl, messagesLimit } from "@/constants";
 import MessagesInbox from "./MessagesInbox";
-import MessagesFooter from "./MessagesFooter";
+import Pagination from "./Pagination";
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const limit = 3;
+  const [hasMoreContent, setHasMoreContent] = useState(false);
 
   useEffect(() => {
     const getMessages = async () => {
       try {
         const response = await getRequest(
-          `${baseUrl}/message/messages?page=${currentPage}&limit=${limit}`
+          `${baseUrl}/message/messages?page=${currentPage}&limit=${messagesLimit}`
         );
         if (response.status === 200 || response.status === 201) {
           setMessages(response.data);
+          setHasMoreContent(response.data.length === messagesLimit);
         }
       } catch (error) {
         console.error("Error fetching messages:", error);
@@ -27,7 +27,9 @@ const Messages = () => {
   }, [currentPage]);
 
   const nextPage = () => {
-    setCurrentPage(currentPage + 1);
+    if (hasMoreContent) {
+      setCurrentPage(currentPage + 1);
+    }
   };
 
   const prevPage = () => {
@@ -37,47 +39,30 @@ const Messages = () => {
   };
 
   return (
-    <>
-      <div id="messages" className="bg-[#1B2426] text-[#D7DADC] m-0 p-0">
-        <MessagesHeader />
-        <div className="flex flex-col justify-center items-center">
-          {messages.map((message, index) => (
-            <MessagesInbox
-              key={index}
-              id={message._id}
-              from={message.from}
-              to={message.to}
-              subject={message.subject}
-              text={message.text}
-              isRead={message.isRead}
-              isDeleted={message.isDeleted}
-              createdAt={message.createdAt}
-            />
-          ))}
-        </div>
-        <div className="flex justify-center items-center w-full mb-[20px]">
-          <div className="flex justify-between items-center w-[30%]">
-            <button
-              className="flex justify-center items-center w-[30px] h-[30px] rounded-full transition-all duration-[200ms] hover:bg-white hover:text-black"
-              onClick={prevPage}
-              disabled={currentPage === 1}
-            >
-              {"<"}
-            </button>
-            <span>Page {currentPage}</span>
-            <button
-              className="flex justify-center items-center w-[30px] h-[30px] rounded-full transition-all duration-[200ms] hover:bg-white hover:text-black"
-              onClick={nextPage}
-              disabled={messages.length < limit}
-            >
-              {" "}
-              {">"}{" "}
-            </button>
-          </div>
-        </div>
-        <MessagesFooter />
+    <div id="messages">
+      <div className="flex flex-col justify-center items-center">
+        {messages.map((message, index) => (
+          <MessagesInbox
+            key={index}
+            id={message._id}
+            from={message.from}
+            to={message.to}
+            subject={message.subject}
+            text={message.text}
+            isRead={message.isRead}
+            isDeleted={message.isDeleted}
+            createdAt={message.createdAt}
+          />
+        ))}
       </div>
-    </>
+
+      <Pagination
+        currentPage={currentPage}
+        hasMoreContent={hasMoreContent}
+        onNextPage={nextPage}
+        onPrevPage={prevPage}
+      />
+    </div>
   );
 };
 
