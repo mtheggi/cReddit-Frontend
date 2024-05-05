846d54028b6bd1b7857e3677d42bb468a0eeb0e2
Master60 <habebelrhman321@gmail.com>
2024-05-05 04:52:46 +0300

    Introducing a new Message Container Window

---
+++ src/Components/messages/Message.jsx
@@ -0,0 +1,40 @@
+import { useLocation } from "react-router-dom";
+import MessagesHeader from "./MessagesHeader";
+import Compose from "./Compose";
+import Inbox from "./Inbox";
+import Unread from "./Unread";
+import Messages from "./Messages";
+import PostReplies from "./PostReplies";
+import UsernameMentions from "./UsernameMentions";
+import Sent from "./Sent";
+import MessagesFooter from "./MessagesFooter";
+
+const Message = () => {
+  const location = useLocation();
+  const pathParts = location.pathname.split("/");
+  const isCompose = pathParts[2] === "compose";
+  const isInbox = pathParts[2] === "inbox";
+  const isUnread = pathParts[2] === "unread";
+  const isMessages = pathParts[2] === "messages";
+  const isPostReplies = pathParts[2] === "selfreply";
+  const isUsernameMentions = pathParts[2] === "mentions";
+  const isSent = pathParts[2] === "sent";
+
+  return (
+    <div id="message" className="bg-[#1B2426] text-[#D7DADC] m-0 p-0">
+      <MessagesHeader />
+
+      {isCompose && <Compose />}
+      {isInbox && <Inbox />}
+      {isUnread && <Unread />}
+      {isMessages && <Messages />}
+      {isPostReplies && <PostReplies />}
+      {isUsernameMentions && <UsernameMentions />}
+      {isSent && <Sent />}
+
+      <MessagesFooter />
+    </div>
+  );
+};
+
+export default Message;
