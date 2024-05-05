1a09541200ccf10f6d825d8d38f5e04057f97d3b
Master60 <habebelrhman321@gmail.com>
2024-05-05 04:53:24 +0300

    Restructuring Compose Page

--- src/Components/messages/Compose.jsx
+++ src/Components/messages/Compose.jsx
@@ -1,19 +1,10 @@
-import MessagesHeader from "./MessagesHeader";
 import MessagesCompose from "./MessagesCompose";
-import MessagesFooter from "./MessagesFooter";
 
 const Compose = () => {
   return (
-    <>
-      <div
-        id="compose"
-        className="bg-[#030303] text-[#D7DADC] min-h-[100%] m-0 p-0"
-      >
-        <MessagesHeader />
-        <MessagesCompose />
-        <MessagesFooter />
-      </div>
-    </>
+    <div id="compose">
+      <MessagesCompose />
+    </div>
   );
 };
 
