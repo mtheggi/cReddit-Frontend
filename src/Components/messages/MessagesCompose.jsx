6351dcb6655b433c46969084f3a64c919200b566
Master60 <habebelrhman321@gmail.com>
2024-05-05 04:57:24 +0300

    Minor Changes in Compose

--- src/Components/messages/MessagesCompose.jsx
+++ src/Components/messages/MessagesCompose.jsx
@@ -18,7 +18,7 @@ const MessagesCompose = () => {
       });
 
       if (response.status === 200 || response.status === 201) {
-        console.log("Message sent successfully");
+        window.location.reload();
       }
     } catch (error) {
       console.error("Error sending message:", error);
@@ -39,62 +39,54 @@ const MessagesCompose = () => {
           onSubmit={handleSubmit}
         >
           <div className="mb-[5px]">
-            <div>
-              <span className="mt-auto capitalize">to</span>
-              <span className="mt-auto text-gray-900"> (username)</span>
-              <div className="mt-[5px] align-top">
-                <input
-                  className="2xs:w-[200px] md:w-[300px] lg:w-[400px] text-[#000000] h-[2.5rem] p-[10px]"
-                  value={to}
-                  onChange={(e) => setTo(e.target.value)}
-                  required
-                />
-              </div>
+            <span className="mt-auto capitalize">to</span>
+            <span className="mt-auto text-gray-900"> (username)</span>
+            <div className="mt-[5px] align-top">
+              <input
+                className="2xs:w-[200px] md:w-[300px] lg:w-[400px] text-[#000000] h-[2.5rem] p-[10px]"
+                value={to}
+                onChange={(e) => setTo(e.target.value)}
+                required
+              />
             </div>
           </div>
 
           <div className="mb-[5px]">
-            <div>
-              <span className="mt-auto capitalize">subject</span>
-              <div className="mt-[5px] align-top">
-                <input
-                  className="2xs:w-[200px] md:w-[300px] lg:w-[400px] text-[#000000] h-[2.5rem] p-[10px]"
-                  value={subject}
-                  onChange={(e) => setSubject(e.target.value)}
-                  required
-                />
-              </div>
+            <span className="mt-auto capitalize">subject</span>
+            <div className="mt-[5px] align-top">
+              <input
+                className="2xs:w-[200px] md:w-[300px] lg:w-[400px] text-[#000000] h-[2.5rem] p-[10px]"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                required
+              />
             </div>
           </div>
 
           <div className="mb-[5px]">
-            <div>
-              <span className="mt-auto capitalize">message</span>
-              <div className="mt-[5px] align-top">
-                <div className="text-[13px]">
-                  <div className="2xs:w-[180px] xs:w-[200px] md:w-[300px] lg:w-[400px] clear-left text-[12px]">
-                    <div className="text-[#d7dadc] text-[1.0769230769230769em] font-normal max-w-[60em]">
-                      <textarea
-                        rows="1"
-                        cols="1"
-                        className="w-full h-[100px] text-black"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        required
-                      />
-                    </div>
-                    <div className="w-[100%]"></div>
+            <span className="mt-auto capitalize">message</span>
+            <div className="mt-[5px] align-top">
+              <div className="text-[13px]">
+                <div className="2xs:w-[180px] xs:w-[200px] md:w-[300px] lg:w-[400px] clear-left text-[12px]">
+                  <div className="text-[#d7dadc] text-[1.0769230769230769em] font-normal max-w-[60em]">
+                    <textarea
+                      rows="1"
+                      cols="1"
+                      className="w-full h-[100px] text-black"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      required
+                    />
                   </div>
+                  <div className="w-[100%]"></div>
                 </div>
               </div>
             </div>
           </div>
 
           <button
-            id="send"
-            name="send"
             type="submit"
-            className="inline-block bg-[#D7DADC] text-[#1A1A1B] uppercase font-semibold cursor-pointer w-[90px] rounded-md shadow-2xl"
+            className="inline-block bg-[#D7DADC] text-[#1A1A1B] uppercase font-semibold cursor-pointer w-[90px] rounded-md border-[2px] hover:drop-shadow-2xl hover:border-black hover:bg-yellow-100"
           >
             send
           </button>
